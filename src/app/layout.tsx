e31b88a12e5d7d681ca0120e2cb05f73ea59e0ba
AntoineCrenshaw <Antoine17343@gmail.com>
2026-06-23 11:45:43 -0500

    Added Auth session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import AuthSessionProvider from "@/components/SessionProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,10 +31,12 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col page-wrapper">
-        <CartProvider>
-          <Navbar />
-          {children}
-        </CartProvider>
+        <AuthSessionProvider>
+          <CartProvider>
+            <Navbar />
+            {children}
+          </CartProvider>
+        </AuthSessionProvider>
       </body>
     </html>
   );
