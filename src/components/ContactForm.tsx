d5f9ec96c3fa59503feb73a78be0cdd2472c593c
AntoineCrenshaw <Antoine17343@gmail.com>
2026-07-30 18:04:06 -0500

    Create JotForm

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,11 @@
+function ContactForm() {
+  return (
+    <iframe
+      title="Contact Us Form"
+      src="https://form.jotform.com/232713977618063"
+      style={{ width: "100%", height: "600px", border: "none" }}
+    />
+  );
+}
+
+export default ContactForm;
