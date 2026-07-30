4a047c3a46d307b1ae02d96117a6893ffdbde4d2
AntoineCrenshaw <Antoine17343@gmail.com>
2026-07-30 18:06:10 -0500

    update middlleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,3 @@
-import { auth } from './lib/auth-edge'
-import { NextResponse } from 'next/server'
-
-export default auth((req) => {
-  const isLoggedIn = !!req.auth
-  const isAdminRoute = req.nextUrl.pathname.startsWith('/admin')
-
-  if (isAdminRoute && !isLoggedIn) {
-    return NextResponse.redirect(new URL('/account', req.url))
-  }
-
-  if (isAdminRoute && req.auth?.user?.role !== 'admin') {
-    return NextResponse.redirect(new URL('/', req.url))
-  }
-
-  return NextResponse.next()
-})
-
 export const config = {
-  matcher: ['/admin/:path*']
+  matcher: []
 }
